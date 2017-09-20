c52e72ec97e283dcd2bc9da6b8cdf4aa853d0343
suffic <wangqingguo.ly@gmail.com>
2017-09-20 17:11:35 +0800

    Initial Commit

---
+++ fool100/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fool100
+//
+//  Created by  on 9/20/17.
+//  Copyright © 2017 zt2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fool100/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fool100
+//
+//  Created by  on 9/20/17.
+//  Copyright © 2017 zt2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
